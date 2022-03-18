f08e31b1e9ade10d1eed23c2c5a49cf499ea955f
Tejas-3098 <47113471+Tejas-3098@users.noreply.github.com>
2022-03-18 01:40:22 -0400

    tuits service tests 3 done

--- a3/src/tests/tuits-service.test.js
+++ a3/src/tests/tuits-service.test.js
@@ -1,15 +1,41 @@
+import {
+createTuit, updateTuit, findTuitById, deleteTuit
+} from "../services/tuits-service";
+
+import {
+createUser,
+  deleteUsersByUsername, findAllUsers,
+  findUserById
+} from "../services/users-service";
+
+
 describe('can create tuit with REST API', () => {
-  // TODO: implement this
-});
+  const tony = {
+     username: 'tonystark',
+     password: 'iamironman',
+     email: 'starktony@marvel.com'
+  };
+  const newTuit = {
+     tuit: "You can take away my house, all my tricks and toys.One thing you can't take away...I am Ironman",
+     postedBy: "tonystark"
+  };
 
-describe('can delete tuit wtih REST API', () => {
-  // TODO: implement this
-});
+  beforeAll(() => {
+     // remove any/all users to make sure we create it in the test
+     return deleteUsersByUsername(tony.username);
+   })
 
-describe('can retrieve a tuit by their primary key with REST API', () => {
-  // TODO: implement this
-});
+  // clean up after test runs
+  afterAll(() => {
+    // remove any data we created
+    return deleteUsersByUsername(tony.username);
+  })
 
-describe('can retrieve all tuits with REST API', () => {
-  // TODO: implement this
-});
+  test('can insert new tuits with REST API', async () => {
+    // insert new user in the database
+    const newUser = await createUser(tony);
+    const createdTuit = await createTuit(newUser._id, newTuit.tuit);
+    //verify that inserted tuit matches parameter tuit
+    expect(createdTuit.tuit).toEqual(newTuit.tuit);
+    expect(createdTuit.postedBy).toEqual(newTuit.postedBy);
+  });
