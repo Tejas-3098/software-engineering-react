164dc11cd2555a20e6ff931d8ae5ba884cb5fdb7
Tejas-3098 <47113471+Tejas-3098@users.noreply.github.com>
2022-03-18 21:36:35 -0400

    aysnc test for tuit list

--- a3/src/tests/tuit-list2.test.js
+++ a3/src/tests/tuit-list2.test.js
@@ -3,6 +3,7 @@ import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllTuits} from "../services/tuits-service";
 
+
 test('tuit list renders async', async () => {
     const tuits = await findAllTuits();
     render(
