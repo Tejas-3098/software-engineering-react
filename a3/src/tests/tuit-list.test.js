2742c1b6e08cdc6660cb821032775c292a88340a
Tejas-3098 <47113471+Tejas-3098@users.noreply.github.com>
2022-03-18 21:34:52 -0400

    tuit-list tests

--- a3/src/tests/tuit-list.test.js
+++ a3/src/tests/tuit-list.test.js
@@ -1,4 +1,4 @@
-import {Tuits} from "../components/tuits";
+import Tuits from "../components/tuits";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllTuits} from "../services/tuits-service";
@@ -7,21 +7,46 @@ import axios from "axios";
 jest.mock('axios');
 
 const MOCKED_USERS = [
-  "alice", "bob", "charlie"
+"alice", "bob", "charlie"
 ];
+
+const NEW_MOCKED_USERS = [
+  {username: 'alice', password: 'alice123', email: 'alice@wonderland.com', _id: "123"},
+  {username: 'bob', password: 'iambob1998', email: 'bob@gmail.com', _id: "155"},
+  {username: 'charlie', password: 'charlie88', email: 'charlie@hotmail.com', _id: "200"}
+]
 
 const MOCKED_TUITS = [
   "alice's tuit", "bob's tuit", "charlie's tuit"
 ];
 
+const NEWLY_MOCKED_TUITS = [
+  {_id: 1, tuit: 'Hello everyone, I am Alice and I am new to tuiter', postedBy: {username: 'alice', password: 'alice123', email: 'alice@wonderland.com', _id: "123"}}, 
+  {_id: 2, tuit: 'Hey! Bob here from Boston', postedBy: {username: 'bob', password: 'iambob1998', email: 'bob@gmail.com', _id: "155"}},
+  {tuit: 'The weather in Boston is so good today!', postedBy: {username: 'charlie', password: 'charlie88', email: 'charlie@hotmail.com', _id: "200"}} 
+]
+
 test('tuit list renders static tuit array', () => {
   // TODO: implement this
+  render(
+    <HashRouter>
+      <Tuits tuits = {NEWLY_MOCKED_TUITS}/>
+    </HashRouter>);
+  const linkElement = screen.getByText(/The weather in Boston is so good today!/i);
+  expect(linkElement).toBeInTheDocument();   
 });
 
-test('tuit list renders async', async () => {
-  // TODO: implement this
-})
-
 test('tuit list renders mocked', async () => {
-  // TODO: implement this
+  axios.get.mockImplementation(() =>
+    Promise.resolve({ data: {tuits: NEWLY_MOCKED_TUITS} }));
+  const response = await findAllTuits();
+  const tuits = response.tuits;
+
+  render(
+    <HashRouter>
+      <Tuits tuits={tuits}/>
+    </HashRouter>);
+
+  const user = screen.getByText(/The weather in Boston is so good today!/i);
+  expect(user).toBeInTheDocument();
 });
